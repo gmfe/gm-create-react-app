671a0c903158b77e9b97008de6dcb3de4a27cde4
liyatang <liyatang@qq.com>
2020-03-05 12:13:19 +0800

    fix

--- packages/scripts/config/webpack.config.js
+++ packages/scripts/config/webpack.config.js
@@ -231,10 +231,10 @@ let config = {
         react:
           isEnvDevelopment &&
           path.resolve(PATH.appDirectory + '/node_modules/react'),
-        'react-dom/server':
-          isEnvDevelopment && require.resolve('@hot-loader/react-dom/server'),
-        'react-dom':
-          isEnvDevelopment && require.resolve('@hot-loader/react-dom'),
+        // 'react-dom/server':
+        //   isEnvDevelopment && require.resolve('@hot-loader/react-dom/server'),
+        // 'react-dom':
+        //   isEnvDevelopment && require.resolve('@hot-loader/react-dom'),
         common: PATH.appDirectory + '/src/js/common/',
         stores: PATH.appDirectory + '/src/js/stores/',
         svg: PATH.appDirectory + '/src/svg/',
