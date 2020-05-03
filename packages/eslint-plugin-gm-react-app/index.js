2044ad23b9b5ec857ec57417f606f86fe00aea7a
LaamGinghong <351390560@qq.com>
2020-05-03 13:08:09 +0800

    fix

--- packages/eslint-plugin-gm-react-app/index.js
+++ packages/eslint-plugin-gm-react-app/index.js
@@ -106,6 +106,12 @@ module.exports = {
                 protected: '^__',
               },
             ],
+            'react/jsx-handler-names': [
+              'error',
+              {
+                eventHandlerPrefix: '_handle',
+              },
+            ],
             'import/extensions': [
               2,
               'ignorePackages',
@@ -120,6 +126,7 @@ module.exports = {
             'import/no-extraneous-dependencies': 'error',
             'react/button-has-type': 0,
             'no-undef': 0,
+            'import/default': 'off',
           },
         },
       ],
