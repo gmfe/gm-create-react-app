f518cf2067158a5a986f3810fe1b66cba4cbb791
LaamGinghong <351390560@qq.com>
2020-07-14 10:24:30 +0800

    Revert "fix"
    This reverts commit 6a1a06bc

--- packages/babel-preset-gm-react-app/index.js
+++ packages/babel-preset-gm-react-app/index.js
@@ -18,7 +18,12 @@ module.exports = (api) => {
       require('@babel/plugin-proposal-nullish-coalescing-operator'),
       require('@babel/plugin-proposal-optional-chaining'),
       require('@babel/plugin-syntax-dynamic-import'),
-      require('@babel/plugin-transform-runtime'),
+      [
+        require('@babel/plugin-transform-runtime'),
+        {
+          corejs: 3,
+        },
+      ],
       require('styled-jsx/babel'),
       require('babel-plugin-lodash'),
       require('react-hot-loader/babel'),
@@ -32,6 +37,8 @@ module.exports = (api) => {
           // for tree shaking
           modules: false,
           useBuiltIns: 'usage',
+          debug: true,
+          corejs: 3,
         },
       ],
       [require('@babel/preset-typescript'), { onlyRemoveTypeImports: true }],
