80122c7b697a12d85df65c0c23bba6898132b039
michwh <fjsxhwh@163.com>
2021-10-14 11:49:14 +0800

    fix: cache配置

--- packages/scripts/config/webpack.config.js
+++ packages/scripts/config/webpack.config.js
@@ -71,11 +71,15 @@ let config = {
       : 'js/[name]/[contenthash:8].chunk.js',
     publicPath: appConfig.publicPath,
   },
+  cache: {
+    type: 'filesystem',
+    // 每当修改了webpack配置，记得更新cache的version，否则可能会出现因为重用了缓存导致配置没生效的问题。
+    version: '3.8.1',
+  },
   optimization: {
     minimize: !isEnvDevelopment,
     minimizer: [
       new TerserPlugin({
-        cache: true,
         parallel: true,
         terserOptions: {
           mangle: false, // Note `mangle.properties` is `false` by default.
