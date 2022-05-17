02e128f3db181645691c766537a08c62cfc44755
michwh <fjsxhwh@163.com>
2022-05-17 17:25:52 +0800

    fix: update module

--- packages/scripts/config/webpack.config.js
+++ packages/scripts/config/webpack.config.js
@@ -78,7 +78,7 @@ let config = {
   cache: {
     type: 'filesystem',
     // 每当修改了webpack配置，记得更新cache的version，否则可能会出现因为重用了缓存导致配置没生效的问题。
-    version: '3.8.1-beta-27.0.0',
+    version: '3.8.1-beta-28.0.0',
   },
   optimization: {
     minimize: !isEnvDevelopment,
