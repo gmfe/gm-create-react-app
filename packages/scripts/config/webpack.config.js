63b5d487b385292c60e354ebda8a00faa3e27c74
michwh <fjsxhwh@163.com>
2022-05-24 17:23:00 +0800

    fix: test

--- packages/scripts/config/webpack.config.js
+++ packages/scripts/config/webpack.config.js
@@ -82,7 +82,7 @@ let config = {
   cache: {
     type: 'filesystem',
     // 每当修改了webpack配置，记得更新cache的version，否则可能会出现因为重用了缓存导致配置没生效的问题。
-    version: '3.8.1-beta-42.0.0',
+    version: '3.8.1-beta-43.0.0',
   },
   optimization: {
     chunkIds: 'natural',
@@ -118,7 +118,7 @@ let config = {
         },
       },
     },
-    runtimeChunk: 'true',
+    runtimeChunk: true,
   },
   module: {
     rules: [
