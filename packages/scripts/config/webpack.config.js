1a8645a86967f905591d9fbfceb587e3faaf4883
michwh <fjsxhwh@163.com>
2022-05-24 19:09:48 +0800

    fix: test

--- packages/scripts/config/webpack.config.js
+++ packages/scripts/config/webpack.config.js
@@ -82,10 +82,11 @@ let config = {
   cache: {
     type: 'filesystem',
     // 每当修改了webpack配置，记得更新cache的version，否则可能会出现因为重用了缓存导致配置没生效的问题。
-    version: '3.8.1-beta-43.0.0',
+    version: '3.8.1-beta-44.0.0',
   },
   optimization: {
     chunkIds: 'natural',
+    moduleIds: 'natural',
     minimize: !isEnvDevelopment,
     minimizer: [
       new TerserPlugin({
@@ -118,7 +119,7 @@ let config = {
         },
       },
     },
-    runtimeChunk: true,
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
