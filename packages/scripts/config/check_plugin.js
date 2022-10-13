7dd4c691c7bca455aaf2b704f4acc671e723d70a
zhongxuhui <zhongxuhui@guanmai.cn>
2022-10-13 11:01:05 +0800

    fix: some

--- packages/scripts/config/check_plugin.js
+++ packages/scripts/config/check_plugin.js
@@ -6,6 +6,11 @@ class CheckPlugin {
           const asset = compilation.getAsset(file)
           if (file.endsWith('.js')) {
             if (asset.source._value) {
+              console.log(
+                'asset.source._value',
+                asset.source._value,
+                asset.source._value.includes('const '),
+              )
               // if (asset.source._value.includes('const ')) {
               //   throw new Error('has const ')
               // }
